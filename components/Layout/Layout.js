f90cd9ba3e8a039e37fbf4979ceb26fdfc3a22bd
kels-orien <kowo4real@yahoo.co.uk>
2020-03-07 22:08:03 +0100

    header styling cont-1

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,8 +1,19 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import css from "../../css/index.scss"
+import Style from "../../css/index.scss"
+
 
 export default function Layout(props) {
+    const handleClick = (e) => {
+        e.preventDefault();
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+     }
+
     return (
         <div>
              <Head>
@@ -12,29 +23,44 @@ export default function Layout(props) {
 for many years with adaptable solutions for ever changing conditions and regulations. Our portfolio
  includes demulsifiers, corrosion inhibitors, sludge breakers, biocides, primary and secondary emulsifiers, and many more. "></meta>
           <title>{props.title}</title>
-          <style dangerouslySetInnerHTML={{__html: css}}/>
+          <style dangerouslySetInnerHTML={{__html: Style}}/>
         </Head>
         
         
 				
 					
 			
-            <header class ="header">
-                <div class="header__container">
-                    <div class="header__container__in"><a class="header__container__in__-1">HUMIGRATI</a></div>
-                    <div class="header__ul">
-                            <div class="header__menu-box">
-                             <li class="header__menu-box__in active">Services</li>   
-                             <li class="header__menu-box__in">Contact Us</li>
-                             <li class="header__menu-box__in">Clients</li>               
-                             <li class="header__menu-box__in">About Us</li>
-                        </div>
+            <header className ="header">
+                
+                <div className="header__container">
+                    <div className="header__container__in"><a className="header__container__in__-1">HUMIGRATI</a></div>
+                    <div className="header__ul">
+                            <div className="header__menu-box">
+                             <li className="header__menu-box__in active">Home</li>   
+                             <li className="header__menu-box__in">Services</li>
+                             <li className="header__menu-box__in">About Us</li>
+                             <li className="header__menu-box__in">Clients</li>               
+                             <li className="header__menu-box__in">Contact Us</li>
+                    </div>
                 </div>
                 </div> 
             </header>
-					
-
-			
+			<header className ="header-mb">
+                <div className="topnav">
+                    <a href="#home" className="active-mb">HUMIGRATI</a>
+                    <div id="myLinks">
+                        <a className = "nav-mb-font" href="#services">Services</a>
+                        <a className = "nav-mb-font" href="#about">About Us</a>
+                        <a className = "nav-mb-font" href="#clients">Clients</a>
+                        <a className = "nav-mb-font" href="#contactus">Contact Us</a>
+                </div>
+                <a  href="" className="icon" onClick={handleClick}>
+                
+                 <span className="burger-mb"><i className="icon ion-md-menu"></i></span>
+                </a>
+                
+                </div>
+            </header>	
         </div>
     )
 }
