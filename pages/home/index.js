6568428ad20e19a5febc897f96ee32252568574d
kels-orien <kowo4real@yahoo.co.uk>
2020-03-26 22:03:21 +0100

    products section edit

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,13 +1,65 @@
 
  function Home() {
     return (
+        <main>
         <div id="banner">
 			    <div className="container">
-                    <section className="description-wrapper" aria-label="Optimising Oil Operations"><h1 className="title">Optimising Oil Operations</h1>
-<p className="title-paragraph">As we produce more oil we encounter new and challenging conditions creating a greater requirement for advanced chemical solutions. Globally, our products help our customers obtain more efficient operations, 
-increased flow assurance, process cost benefits and improved environmental profile</p></section>
+                    <section className="description-wrapper" aria-label="Optimising Oil Operations">
+                        <div className="col-xs-12">
+                            <h1 className="title">Optimising Oil Operations</h1>
+                            <div className="oil-operations"><p className="title-paragraph">
+                            Humigrati has been extensively involved in  oil service projects specifically in the areas of effluent water monitoring,
+                             crude oil and crude characterization analysis, bottle tests for selection of effective demulsifiers, water clarifier, paraffin inhibitor, 
+                             wax dissolving chemicals, microbiocides and other oil field chemicals. The company through research has developed
+                              and manufactured effective chemicals for use in the oil industry for over twenty five years and have experienced field application engineers.
+                            </p></div>
+                        <div className="btn-div">
+                            <button className ="btn">Contact Us</button>
+                        </div>
+                            
+                        </div>
+                    </section>
 			    </div>
-		    </div>
+		</div>
+        <article className="child-page">
+        <section className="our-products" aria-label="Our Products demulsifiers, corrosion inhibitors scale inhibitors oil spill dispersant degreasers paraffin inhibitors paraffin solvents water clarifiers Quat biocide, THPS">
+            <div className="our-products-header">
+                <h2 className="section-title">Our Products</h2>
+                <p  className="section-paragraph">Buy our tailor made chemicals solutions</p>
+
+                <div>
+
+                    <ul className="chemical-list">
+                            <li><a  href="#"className="chemical-list-button">Corrosion Inhibitors</a></li>
+                            <li><a  href="#"className="chemical-list-button">Defoamers</a></li>
+                            <li><a  href="#"className="chemical-list-button">Degreasers</a></li>
+                            <li><a  href="#"className="chemical-list-button">Demulsifiers</a></li>
+                            <li><a  href="#"className="chemical-list-button">Dewaxers</a></li>
+                            <li><a  href="#"className="chemical-list-button">Dispersants</a></li>
+                            <li><a href="#"className="chemical-list-button">Emulsifiers</a> </li>
+                            <li><a href="#"className="chemical-list-button">Microbiocides</a></li>
+                            <li><a href="#"className="chemical-list-button">Paraffin Inhibitors</a></li>
+                            <li><a href="#"className="chemical-list-button">Paraffin Solvents</a></li>
+                            <li><a href="#"className="chemical-list-button">Polyelectrolyte</a></li>
+                            <li><a href="#"className="chemical-list-button">Scale Inhibitors</a></li>
+                            <li><a href="#"className="chemical-list-button">Sludge Breakers</a></li>
+                            <li><a href="#"className="chemical-list-button">Water Clarifier</a></li>
+                            <li><a href="#"className="chemical-list-button">Wetting Agents</a></li>
+                    </ul>
+                </div>
+            </div>
+            
+        
+        </section>
+
+
+
+        </article>
+        
+
+        <section className="about-us-home"></section>
+        </main>
+        
     )
 }
 
