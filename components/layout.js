21efff5cd389be19670458aeb85ec8d8dbf78a0a
Kohwo Orien <kowo4real@yahoo.co.uk>
2020-08-07 12:24:09 +0100

    checking a tags

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,4 @@
-import Head from '../node_modules/next/head'
+import NextHead from 'next/head';
 import Link from 'next/link';
 import Style from "../css/index.scss"
 
@@ -16,11 +16,12 @@ const Layout = props => {
 
     return (
         <React.Fragment>
-            <Head>
+            <NextHead>
+            <title>{props.title}</title>
                 <meta charSet="UTF-8" />
                 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <title>{props.title}</title>
+               
                 <meta name="description" content="At Humigrati, we have been providing chemicals to the oilfield production market 
                 for many years with adaptable solutions for ever changing conditions and regulations. Our portfolio
                 includes demulsifiers, corrosion inhibitors, sludge breakers, biocides, primary and secondary emulsifiers, and many more. "/>
@@ -28,17 +29,28 @@ const Layout = props => {
                  drilling chemicals, petrochemical products, crude oil analysis, defoamers,foamers,biocides,corrosion,inhibitor,emulsifier,
                  solvent,scale,surfacants,demulsifier,paraffin,scavengers,
                 hydrate,chemistries,chemistry,engineer, effluent water monitoring, and laboratory services, procurement"/>
+
                  <meta name="author" content="Humigrati Chemical Company Limited"/>
+
                  <meta property="og:url" content="https://www.humigrati.com"/>
+
+                 <meta property="og:locale" content="en_US" />
                  <meta property="og:type" content="website"></meta>
                  <meta property="og:description" content="At Humigrati, we have been providing chemicals to the oilfield production market 
                 for many years with adaptable solutions for ever changing conditions and regulations. Our portfolio
                 includes demulsifiers, corrosion inhibitors, sludge breakers, biocides, primary and secondary emulsifiers, and many more."/>
+
                  <meta name="brand" content="Humigrati Chemical Company Limited"/>
+
+                <meta name="twitter:card" content="Humigrati Chemical Company Limited" />
+                <meta name="twitter:title" content="" />
+                <meta name="twitter:description" content="Humigrati Chemical Company Limited, humigrati,chemicals,chemical,oil,gas, production" />
+
                  <meta name="google-site-verification" content="zVFJffwpq1FcUMLrdxYOqoqXSjWJ9i3FVsiwIqSkxRM" />
+
                 <link rel="icon" href="/assets/images/favicon.ico" />
                 <style dangerouslySetInnerHTML={{ __html: Style }} />
-            </Head>
+            </NextHead>
 
 
 
@@ -49,8 +61,8 @@ const Layout = props => {
                         <div className="header__container__in"><Link href="/"><a className="header__container__in__-1">HUMIGRATI</a></Link></div>
                         <div className="header__ul">
                             <ul className="header__menu-box" >
-                                <Link href="/"><li className="header__menu-box__in active"><a>Home</a></li></Link>
-                                <Link href="/service"><li className="header__menu-box__in"><a>Services</a></li></Link>
+                                <Link href="/"><li className="header__menu-box__in active"><a href="/about">Home</a></li></Link>
+                                <Link href="/service"><li className="header__menu-box__in">Services</li></Link>
                                 <Link href="/about"><li className="header__menu-box__in"><a>About Us</a></li></Link>
                                 <Link href="/client"><li className="header__menu-box__in"><a>Clients</a></li></Link>
                                 <Link href="/contact"><li className="header__menu-box__in"><a>Contact Us</a></li></Link>
